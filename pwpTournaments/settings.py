de6f443a3a8c42df272af7e250d4a0dbc410a856
Adrian <niedziolkadadrian@gmail.com>
2023-01-25 20:16:47 +0100

    Zmieniono styl logowania i rejestracji

--- pwpTournaments/settings.py
+++ pwpTournaments/settings.py
@@ -9,6 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+"""
+DANE LOGOWANIA:
+USERNAME:   PASSWORD:
+admin       admin
+inny        zaq1@WSX
+"""
 import os
 from pathlib import Path
 
